d5ca17ff164e297b28df067bf0f5a930afd96e3e
mbird14241 <mbird14241+github@gmail.com>
2016-02-20 23:22:03 -0500

    Added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
 ## This file implements some helper functions for dealing
 ## with matrices andf caching their inverses
 
+## Important: if the user passed additional arguments to cacheSolve() and
+## then called it again with other additional arguments, it would return
+## the original cached matrix, not one calculated with with the second 
+## set of additional arguments. That would be a bug.
+
+
 ## This function "makes" a matrix capable of caching its inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
+    
+    # Cache the last set of arguments in addition to the last inverse 
     lastArgs <- NULL
     set <- function(y) {
         x <<- y
@@ -32,6 +40,10 @@ cacheSolve <- function(x, ...) {
     i <- x$getInverse()
     lastArgs <- x$getLastArgs()
     thisArgs <- c(...)
+    
+    ## if there is a matrix and the additional arguments 
+    ## passed last time match, return the cached inverse.
+    ## Otherwise recompute.
     if(!is.null(i) && 
        length(thisArgs) == length(lastArgs) &&
        all(thisArgs == lastArgs))
@@ -42,6 +54,10 @@ cacheSolve <- function(x, ...) {
     data <- x$get()
     i <- solve(data)
     x$setInverse(i)
+    
+    ## Also, cache the args that we calculated with.
     x$setLastArgs(thisArgs)
     i
 }
+
+
